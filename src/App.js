ee04f974485ee4c3dcfaf44c81283f4a7f425504
andyafandi <andy_afandi@yahoo.com>
2025-02-02 14:47:56 +0700

    Edit App.js dan App.css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import the CSS file for styling
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="blinking-text">Welcome to React</h1>
+        <h3> Halaman ini sudah dimodifikasi sedikit, selamat menikmati!</h3>
+      </header>
+    </div>
+  );
 }
 
 export default App;
