efd08b56b6db9b49c9eeacc6af32baba8d8f0bda
andyafandi <andy_afandi@yahoo.com>
2025-02-02 18:06:03 +0700

    Edit App.js v2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css'; // Import the CSS file for styling
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="blinking-text">Welcome to React</h1>
-        <h3> Halaman ini sudah dimodifikasi sedikit, selamat menikmati!</h3>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+      // <header className="App-header">
+      //   <h1 className="blinking-text">Welcome to React</h1>
+      //   <h3> Halaman ini sudah dimodifikasi sedikit, selamat menikmati!</h3>
+      // </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="blinking-text">Welcome to React</h1>
+          <h3> Halaman ini sudah dimodifikasi sedikit, selamat menikmati!</h3>
+        </header>
+
+      </div>
+    );
+  }
 }
 
 export default App;
