48c7c8344aae9858963638b6856c01ea8e9ead24
andyafandi <andy_afandi@yahoo.com>
2025-02-05 11:30:22 +0700

    edit APP.JS

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
+          <h1 className="App-title">Selamat Datang di Proyek Akhir Membangun CI/CD Pipeline</h1>
         </header>
+        <section>
+          <h2>ID Camp 2024 - Level Menengah</h2>
+          <h3>by: Mohamad Irwam Afandi</h3>
+        </section>
         <p className="App-intro">
-          Halaman ini sudah dimodifikasi untuk Proyek Akhir
-          
+          Ini adalah halaman utama proyek akhir untuk membangun pipeline CI/CD menggunakan teknologi terbaru. Di halaman ini, Anda dapat melihat pengenalan tentang proyek ini dan langkah-langkah untuk memulai.
         </p>
       </div>
     );
